c3504806b4996ca078d99a09f591b4220c76f327
Michael Windell <michael@stormlightstudio.co.uk>
2023-03-07 09:06:54 +0000

    trustpilot verification

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;700&family=Open+Sans:wght@400;700&display=swap"
             rel="stylesheet"
           />
+          <meta
+            name="trustpilot-one-time-domain-verification-id"
+            content="5fd5ed5d-1faf-4866-8e4f-c25b2f8d06dc"
+          />
         </Head>
         <body>
           <Main />
